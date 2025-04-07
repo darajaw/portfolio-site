47fa56b5392917afb0f8ed89e2e91782b0bdd701
Daraja Williams <will1158@algonquinlive.com>
2025-04-07 15:45:32 -0400

    inital set up

---
+++ src/app/Header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+    return (
+<header className="px-20">
+<h1>Daraja Williams</h1>
+
+<p className="mb-6">Full Stack Developer</p>
+
+<nav>
+  <a href="/skills">Home</a>
+  <a>Skills</a>
+  <a>Education</a>
+  <a>Hobbies</a>
+  <a>About</a>
+  <a>Contact</a>
+  <a>Resume</a>
+</nav>
+<hr></hr>
+</header>
+    );}
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="grid grid-rows-[1fr_20px] min-h-screen ml-10 mr-10 p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-      <main className=" px-20 pt-8 content-center items-center  sm:items-start">
+      <main className="flex flex-col pt-8 row-start-1 content-center items-center sm:items-start">
         
-        <h2 className="mb-5">Hi There!</h2>
+        <h2 className="mb-5">Skills!</h2>
         <p>Thank you for visiting my website. I am a Full Stack Developer with expertise in various technologies. Feel free to explore my skills, education, and hobbies through the navigation menu. As I continue to add new features to enhance this website, please feel free to send me your feedback!</p>
       
       </main>
 
-      <footer className=" flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-2 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
